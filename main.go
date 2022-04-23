537976ec415c0c73e3b0c9410fd64f54bbc03650
crazyJUMP <crazyjuump@gmail.com>
2022-04-24 01:44:33 +0700

    added routes

--- main.go
+++ main.go
@@ -6,20 +6,45 @@ import (
 	"net/http"
 )
 
-// Handler
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%T", r)
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// Same thing as above
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a new snippet..."))
+}
+
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 	
 	port := 4000
 	log.Printf("Starting server on: %d", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
 	log.Fatal(err)
-
-	
 }
